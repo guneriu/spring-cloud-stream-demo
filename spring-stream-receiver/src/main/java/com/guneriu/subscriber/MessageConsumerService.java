25481e6699673e535dd0bd2fc5359ef32377cb39
guneriu <guneriu@gmail.com>
2016-09-18 14:10:50 +0300

    enabled dlq and reprocessing

--- spring-stream-receiver/src/main/java/com/guneriu/subscriber/MessageConsumerService.java
+++ spring-stream-receiver/src/main/java/com/guneriu/subscriber/MessageConsumerService.java
@@ -1,12 +1,11 @@
 package com.guneriu.subscriber;
 
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import com.guneriu.component.CustomerEvent;
 import com.guneriu.component.TaskMessage;
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-
-import lombok.extern.slf4j.Slf4j;
 
 @EnableBinding(CustomerEvent.class)
 @Slf4j
@@ -14,11 +13,11 @@ public class MessageConsumerService {
 
     @StreamListener(CustomerEvent.STATUS_CHANGE)
     public void handle(TaskMessage message) {
-//        if (Integer.valueOf(message.getValue()) % 2 == 0) {
+        if (Integer.valueOf(message.getValue()) % 2 == 0) {
             log.info("received message {}", message);
-//        } else {
-//            throw new RuntimeJsonMappingException("hedelek");
-//        }
+        } else {
+            throw new RuntimeJsonMappingException("hedelek");
+        }
     }
 
 }
